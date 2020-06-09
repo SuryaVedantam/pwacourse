d79b650be418c2972bfae5980f909ba1617df90a
SuryaVedantam <61188060+SuryaVedantam@users.noreply.github.com>
2020-06-09 16:54:15 +0530

    installing activating

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
 
+self.addEventListener('install',function(event){
+  console.log("[ServiceWorker] Installing service worker...", event);
+});
+
+self.addEventListener('activate',function(event){
+  console.log("[ServiceWorker] Activating service worker...", event);
+  return self.clients.claim();
+});
