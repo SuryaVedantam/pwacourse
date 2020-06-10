3069eba254e7bb932381c0942bfd8ca428d8271c
SuryaVedantam <61188060+SuryaVedantam@users.noreply.github.com>
2020-06-10 12:40:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
 
 self.addEventListener('install',function(event){
   console.log("[ServiceWorker] Installing service worker...", event);
+  event.waitUntil(
+  caches.open('static').then(function(cache){
+       console.log("[ServiceWorker] Pre caching...", cache);
+      cache.add('/pwacourse/app.js');
+  });
+  );
+  
 });
 
 self.addEventListener('activate',function(event){
